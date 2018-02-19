5007d32a9524394637d4c978404167baf20559eb
Imran Khan <imran.khan@kpn.com>
2018-02-19 10:56:27 +0100

    base setup

--- gamecontrol/gamecontrol.component.ts
+++ gamecontrol/gamecontrol.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gamecontrol.component.css']
 })
 export class GamecontrolComponent implements OnInit {
-  increment: number;
 
   constructor() { }
 
